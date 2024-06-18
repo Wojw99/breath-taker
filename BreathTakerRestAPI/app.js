a8e24f05c5209acdff4e331b6ebe23223ccf362e
Wojw99 <woj199934@gmail.com>
2024-06-18 15:24:33 +0200

    last addings

--- BreathTakerRestAPI/app.js
+++ BreathTakerRestAPI/app.js
@@ -4,9 +4,9 @@ const port = 3000;
 
 const articles = [
     { id: 1, title: 'How to use the app?', text_read: '5 min read', icon_tag : 'help', content : [
-        ".",
-        ".",
-        ".",
+        "To start the breathing exercise, click on the 'Default Training' button. Duration of breath phases are calculate based on your height and sex. Then rate your mood by choosing 1 to 5 value. Next you will be asked to breathe in and out for a certain amount of time. Take your breaths according to visual progress bar and sound cues.",
+        "Alternatively you can adjust duration of breath phases by clicking 'Adjustment' button. You will be asked to start breathing and stop timer 4 times after each phase of breath (inhale, hold, exhale, hold).",
+        "Click 'Custom Training' to train breathing using your adjusted values. You will be also asked to rate your mood to calculate only the duration of the whole exercise.",
     ] }, 
     { id: 2, title: 'How to breath correctly?', text_read: '5 min read', icon_tag : 'breath', content : [
         'Take breath using your nose. Sometimes breathing through the mouth is necessary, but generally breathing through your nose helps in preventing infections and face deformations.',
